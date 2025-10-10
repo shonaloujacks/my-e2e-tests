ede0baa178659ff373ab23f0cb5b0f14ba22a926
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-10-10 15:26:03 +0100

    user log in test added

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -8,4 +8,18 @@ describe('Note app', () => {
     await expect(locator).toBeVisible()
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
 })
+
+ test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByLabel('Username').fill('mluukkai')
+    await page.getByLabel('Password').fill('salainen')
+    
+    await page.getByRole('button', { name: 'login' }).click()
+  
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+
+  })
+
 })
