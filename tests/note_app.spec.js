4ea4ff1b1650945f65aed05b4c8cd3c61b3fe47e
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2025-10-16 12:01:05 +0100

    base url added

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -3,15 +3,15 @@ const { loginWith, createNote } = require('./helper')
 
 describe('Note app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('http://localhost:3001/api/testing/reset')
-    await request.post('http://localhost:3001/api/users', {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
       data: {
         name: 'Matti Luukkainen',
         username: 'mluukkai',
         password: 'salainen'
       }
     })
-     await page.goto('http://localhost:5173')
+     await page.goto('/')
     })
 
   test('front page can be opened', async ({ page }) => {
